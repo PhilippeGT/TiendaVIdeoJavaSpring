83b7ac5ca3955e37a5a61d21c11014fc4e82e538
PhilippeGT <philippe.trivin@gmail.com>
2022-09-18 11:06:53 -0500

    actualizacion

---
+++ apitiendavideo/src/main/java/tiendavideo/apitiendavideo/repositorio/TerceroRepositorio.java
@@ -0,0 +1,10 @@
+package tiendavideo.apitiendavideo.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tiendavideo.apitiendavideo.modelo.Tercero;
+
+@Repository
+public interface TerceroRepositorio extends JpaRepository<Tercero, Long> {
+
+}
